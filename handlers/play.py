fd8bfaa6224f1ce3c7e74c56c4261a230d76495b
SOCIAL MECHANIC <95063433+SOCIAL-MECHANIC-1997@users.noreply.github.com>
2022-05-01 07:53:58 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -92,7 +92,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
     draw.text((190, 630), f"Views: {views}", (255, 255, 255), font=font)
     draw.text(
         (190, 670),
-        f"Powered By:  𝙎𝙊𝘾𝙄𝘼𝙇 𝙈𝙀𝘾𝙃𝘼𝙉𝙄𝘾",
+        f"Powered By: social mechanic",
         (255, 255, 255),
         font=font,
     )
