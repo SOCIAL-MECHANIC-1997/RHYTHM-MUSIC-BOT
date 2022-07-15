60998f39eef381841e733c08a1b266bd9c7ac230
SOCIAL MECHANIC <95063433+SOCIAL-MECHANIC-1997@users.noreply.github.com>
2022-07-15 18:35:49 +0530

    Update __main__.py

--- modules/__main__.py
+++ modules/__main__.py
@@ -1,9 +1,9 @@
 from pyrogram import idle
 from pyrogram import Client as Bot
-from modules.callsmusic import run
+from modules.clientbot import run
 from modules.config import API_ID, API_HASH, BOT_TOKEN
 
-
+    
 bot = Bot(
     ":memory:",
     API_ID,
