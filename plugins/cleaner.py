3b6126ea847a851dbdb864209b4c28a8629a1a72
SOCIAL MECHANIC <95063433+SOCIAL-MECHANIC-1997@users.noreply.github.com>
2022-07-15 18:37:59 +0530

    Update cleaner.py

--- plugins/cleaner.py
+++ plugins/cleaner.py
@@ -17,12 +17,12 @@ async def clear_downloads(_, message: Message):
     if ls_dir:
         for file in os.listdir(downloads):
             os.remove(os.path.join(downloads, file))
-        await message.reply_text("✅ **𝑫𝒆𝒍𝒆𝒕𝒆𝒅 𝑨𝒍𝒍 𝑫𝒐𝒘𝒏𝒍𝒐𝒂𝒅𝒆𝒅 𝑭𝒊𝒍𝒆𝒔 ...**")
+        await message.reply_text("✅ **ᴅᴇʟᴇᴛᴇᴅ ᴀʟʟ ᴅᴏᴡɴʟᴏᴀᴅ ғɪʟᴇs**")
     else:
-        await message.reply_text("❌ **𝑵𝒐 𝑭𝒊𝒍𝒆𝒔 𝑫𝒐𝒘𝒏𝒍𝒐𝒂𝒅𝒆𝒅 ...**")
+        await message.reply_text("❌ **ɴᴏ ғɪʟᴇs ᴅᴏᴡɴʟᴏᴀᴅᴇᴅ**")
 
         
-@Client.on_message(command(["rmr", "clean"]) & ~filters.edited)
+@Client.on_message(command(["rmw", "clean"]) & ~filters.edited)
 @errors
 @sudo_users_only
 async def clear_raw(_, message: Message):
@@ -30,9 +30,9 @@ async def clear_raw(_, message: Message):
     if ls_dir:
         for file in os.listdir(raw_files):
             os.remove(os.path.join(raw_files, file))
-        await message.reply_text("✅ **𝑫𝒆𝒍𝒆𝒕𝒆𝒅 𝑨𝒍𝒍 𝑹𝒂𝒘 𝑭𝒊𝒍𝒆𝒔 ...**")
+        await message.reply_text("✅ **ᴅᴇʟᴇᴛᴇᴅ ᴀʟʟ ʀᴀᴡ ғɪʟᴇs**")
     else:
-        await message.reply_text("❌ **𝑵𝒐 𝑹𝒂𝒘 𝑭𝒊𝒍𝒆𝒔 𝒊𝒏 𝑺𝒆𝒓𝒗𝒆𝒓 ...**")
+        await message.reply_text("❌ **ɴᴏ ʀᴀᴡ ғɪʟᴇs**")
 
 
 @Client.on_message(command(["cleanup"]) & ~filters.edited)
@@ -44,6 +44,6 @@ async def cleanup(_, message: Message):
     if ls_dir:
         for dta in os.listdir(pth):
             os.system("rm -rf *.webm *.jpg")
-        await message.reply_text("✅ **𝑪𝒍𝒆𝒂𝒏𝒆𝒅 𝑨𝒍𝒍 𝑱𝒖𝒏𝒌𝒔 𝑻𝒉𝒖𝒎𝒏𝒂𝒊𝒍𝒔 ...**")
+        await message.reply_text("✅ **ᴄʟᴇᴀɴᴇᴅ**")
     else:
-        await message.reply_text("✅ **𝑨𝒍𝒓𝒆𝒂𝒅𝒚 𝑪𝒍𝒆𝒂𝒏𝒆𝒅 𝑨𝒍𝒍 𝑱𝒖𝒏𝒌𝒔 ...**")
+        await message.reply_text("✅ **ᴀʟʀᴇᴀᴅʏ ᴄʟᴇᴀɴᴇᴅ**")
